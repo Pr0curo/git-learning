976f3da80f983be334b72612059da1fcfd2edb2c
Pr0curo <jhelbig86@gmail.com>
2015-12-27 12:30:56 +0100

    corrected counting once more

--- test1.cpp
+++ test1.cpp
@@ -4,11 +4,11 @@ int main()
 {
     std::cout << "count to ten" << std::endl;
 
-    int count(10);
+    int count(11);
 
-    for( int counter=0; counter < count; counter++ )
+    for( int counter=1; counter < count; counter++ )
     {
-        std::cout << counter+1 << std::endl;
+        std::cout << counter << std::endl;
     }
 
     std::cout << "counting finished" << std::endl;
